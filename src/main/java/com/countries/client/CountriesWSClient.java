1b63f44ac38423b30ab5e27002106e944ac8d5fa
Łukasz Sieczkowski <sieczkowski.l@gmail.com>
2017-08-28 13:25:25 +0200

    .

--- src/main/java/com/countries/client/CountriesWSClient.java
+++ src/main/java/com/countries/client/CountriesWSClient.java
@@ -1,9 +1,5 @@
 package com.countries.client;
 
-import javax.jws.HandlerChain;
-import javax.xml.ws.WebServiceClient;
-
-
 public class CountriesWSClient {
 
 	public static void main(String[] args) {
